b42009bc7fd3cd709672de65435185d6e9206ca3
Lintangms14 <merayusukmalintang@gmail.com>
2023-06-22 11:16:35 +0700

    fix eror

--- CircularQueue/CircularQueue.cpp
+++ CircularQueue/CircularQueue.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
-class queue {
+class queues {
 	int FRONT, REAR, max = 5;
 	int queue_array[5];
 
 public:
-	queue() {
+	queues() {
 		FRONT = -1;
 		REAR = -1;
 	}
